38d4b96af227f0038e91d529b5f4b15a0c5ec753
MrKrukov <krukov.alex.lg.spirit@gmail.com>
2022-03-15 21:38:14 +0300

    first commit

---
+++ cmake-build-debug/CMakeFiles/laba5c.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/laba5c.dir/main.c.obj"
+  "laba5c.exe"
+  "laba5c.exe.manifest"
+  "laba5c.pdb"
+  "liblaba5c.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/laba5c.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
